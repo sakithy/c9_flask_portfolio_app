ead206907d92402be7e2b16f46b678b816a86446
MD S Amin <32438531+sakithy@users.noreply.github.com>
2019-09-06 21:44:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def time_post():
           print(request.form['text'].split())
           
           for item in request.form['text'].split():
-            answer = (datetime.datetime.now(pytz.timezone("US/Eastern")).strftime('Time = ' + '%H:%M:%S' + ' GMT ' + ' Year = ' + '%d-%m-%Y'))
+            answer = (datetime.datetime.now(pytz.timezone("US/Eastern")).strftime('Time = ' + '%H:%M:%S' + ' EST ' + ' Year = ' + '%m-%d-%Y'))
             #answer = datetime.datetime.now().strftime('Time == ' + '%H:%M:%S' + ' Year == ' + '%d-%m-%Y')
             #answer = datetime.datetime.now().strftime('%Y-%m-%d \n %H:%M:%S')
 
